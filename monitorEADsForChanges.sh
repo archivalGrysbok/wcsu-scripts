b7e4dbe75bc2c899c85bd7d0eb7ad48a8324bd70
archivalGrysbok <archivalGrysbok@github.com>
2020-05-21 14:51:09 -0400

    Moved EADs to a new directory. Updated monitor script accordingly.

--- monitorEADsForChanges.sh
+++ monitorEADsForChanges.sh
@@ -1,3 +1,3 @@
 source /home/arclight/.bash_profile
 
-nohup /usr/local/bin/fswatch -r /home/arclight/arclight/data/ead/ -t -x --event Created --event Updated >> /home/arclight/scripts/arclight_fileChanges.txt &
+nohup /usr/local/bin/fswatch -r /var/www/html/arclight/data/ead/ -t -x --event Created --event Updated >> /home/arclight/scripts/arclight_fileChanges.txt &
