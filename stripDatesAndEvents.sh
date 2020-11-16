b85841bc7f9209388787ac900790775b939dcf30
archivalGrysbok <archivalGrysbok@github.com>
2020-11-16 09:37:51 -0500

    moved the remove-line-from-input-file to after it index the file

--- stripDatesAndEvents.sh
+++ stripDatesAndEvents.sh
@@ -2,23 +2,25 @@ source /home/arclight/.bash_profile
 
 cd /home/arclight/scripts
 
-file=arclight_fileChangesSorted.txt
+file=/home/arclight/scripts/arclight_fileChangesSorted.txt
 
+echo "sorting file changes"
 sort -u /home/arclight/scripts/arclight_fileChanges.txt >> /home/arclight/scripts/arclight_fileChangesSorted.txt
 
 rm /home/arclight/scripts/arclight_fileChanges.txt
 
+echo "killing fswatch"
 killall fswatch
 
-/usr/local/bin/fswatch -r -L /var/www/html/arclight/data/ead/  --event Created  --event Updated >> /home/arclight/scripts/arclight_fileChanges.txt
+echo "starting fswatch"
+/usr/local/bin/fswatch -r -L /var/www/html/arclight/data/ead/  --event Created  --event Updated >> /home/arclight/scripts/arclight_fileChanges.txt &
 
+echo "starting indexing loop"
 while read -r line
 do
-  cd /home/arclight/scripts
+  cd /home/arclight/arclight/
 
-  head -n 1  "$file"  >> processed.txt   #copies first line to backup file
-
-  sed -i -e "1d" $file   # deletes first line
+  head -n 1  "$file"  >> /home/arclight/scripts/processed.txt   #copies first line to backup file
 
   runthis=$(echo $line | sed -r s',^(\/.*\/(.*)\/.*)+,FILE=\1 REPOSITORY_ID=\2 bundle exec rake arclight:index,')
 
@@ -30,8 +32,17 @@ do
 
   eval $runthis
 
+  echo "going back to scripts directory"
+  cd /home/arclight/scripts
+
+  sed -i -e "1d" $file   # deletes first line
+
+  cd /home/arclight/arclight/
+
   line=
 
   runthis=
 
 done < /home/arclight/scripts/arclight_fileChangesSorted.txt
+
+echo "all done :)"
