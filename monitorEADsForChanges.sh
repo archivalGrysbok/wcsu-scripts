bd2f534998b73ee71be8a246b29abf6482b8a901
archivalGrysbok <archivalGrysbok@github.com>
2020-11-16 09:38:36 -0500

    removed because it is now incorporated into the crontab as a long, long line

--- monitorEADsForChanges.sh
+++
@@ -1,7 +0,0 @@
-source /home/arclight/.bash_profile
-
-/usr/local/bin/fswatch -r -L /var/www/html/arclight/data/ead/  --event Updated >> /home/arclight/scripts/arclight_fileChanges.txt 
-
-#nohup /usr/local/bin/fswatch -r -L -0 /var/www/html/arclight/data/ead/ --event Created --event Updated | xargs -0 -n 1 -I {} echo {} >> /home/arclight/scripts/arclight_fileChanges.txt
-
-
